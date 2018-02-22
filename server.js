2f3f9f7039b60efc2a9b5001ebbbc4b7603734c6
Rebecca Popper <rpopper7@gmail.com>
2018-02-22 17:36:26 -0500

    set up

---
+++ server.js
@@ -0,0 +1,2 @@
+// https://www.lynda.com/Express-js-tutorials/Setup-configurations/533304/557605-4.html?srchtrk=index%3a1%0alinktypeid%3a2%0aq%3ajavascript+full+stack%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2
+// 2. Node as a web server is my current video 
